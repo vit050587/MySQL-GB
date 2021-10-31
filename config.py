bb8b2082d8999f3166f277623a0dd9437298f534
Vitaly <vit050587@mail.ru>
2021-10-31 22:00:45 +0300

    Initial commit

---
+++ config.py
@@ -0,0 +1,4 @@
+host = "localhost"
+user = "root"
+password = "R@ndoom$1"
+db_name = "sakila"
---
+++ main.py
@@ -0,0 +1,17 @@
+import pymysql
+from config import host, user, password, db_name
+
+try:
+    connection = pymysql.connect(
+        host = host,
+        port = 3306,
+        user = user,
+        password = password,
+        database = db_name,
+        cursorclass = pymysql.cursors.DictCursor
+    )
+    print("Успешное подключение...!")
+except Exception as ex:
+    print("Нет подключения...!")
+    print(ex)
+
